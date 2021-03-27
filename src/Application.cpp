d890e383e8ad90079aa25c259301a4634c710625
Anshul7sp1 <Anshul7sp1@users.noreply.github.com>
2021-03-27 12:58:11 +0530

    Added error logging for OpenGL API calls

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,6 +4,28 @@
 #include <fstream>
 #include <sstream>
 
+//------------------------------------MACROS-----------------------------------
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define CallWithLog(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+//---------------------------------ERROR LOGGING--------------------------------
+
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+static bool GLLogCall(const char* function, const char* file, int line) {
+    GLenum error = glGetError();
+    if (error) {
+        std::cout << "[OpenGL Error](" << error << ")\n[Function]" << function<< "\n[File]" << file << "\n[Line]" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
+//---------------------------------SHADER PROGRAM-------------------------------
 
 //A shader source object that contains vertex and fragment shader source code.
 struct ShaderProgramSource {
@@ -45,18 +67,18 @@ static ShaderProgramSource ParseShader(const std::string& filepath) {
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
     unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+    CallWithLog(glShaderSource(id, 1, &src, nullptr));
+    CallWithLog(glCompileShader(id));
 
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    CallWithLog(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
     if (result == GL_FALSE) {
         int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        CallWithLog(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
         char* message = (char*)_malloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
+        CallWithLog(glGetShaderInfoLog(id, length, &length, message));
         std::cout << "Failed to compile " << type << " shader\n" << "Info: "<< message <<std::endl;
-        glDeleteShader(id);
+        CallWithLog(glDeleteShader(id));
         return 0;
     }
 
@@ -70,13 +92,13 @@ static unsigned int CreateShaderProgram(const std::string& vertexShader, const s
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
+    CallWithLog(glAttachShader(program, vs));
+    CallWithLog(glAttachShader(program, fs));
+    CallWithLog(glLinkProgram(program));
+    CallWithLog(glValidateProgram(program));
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    CallWithLog(glDeleteShader(vs));
+    CallWithLog(glDeleteShader(fs));
 
     return program;
 }
@@ -145,7 +167,7 @@ int main(void)
     //Main loop the draws on screen.
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        CallWithLog(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
         glfwSwapBuffers(window);  
         glfwPollEvents();
     }
