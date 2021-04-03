09fe54383aac5c115ed3f7ec40c077f928df0570
Anshul7sp1 <Anshul7sp1@users.noreply.github.com>
2021-04-03 21:33:03 +0530

    Added functions and changed main.cpp to reflect texture changes

--- src/Application.cpp
+++ src/Application.cpp
@@ -9,6 +9,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 
 //--------------------------------------MAIN------------------------------------
@@ -34,21 +35,28 @@ int main(void)
     {
         //vertex and index buffers.
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f
+            -0.5f, -0.5f, 0.0f, 0.0f,
+             0.5f, -0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f, 1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f, 1.0f
         };
         unsigned int indices[] = {
             0, 1, 2,
             2, 3, 0
         };
+
+        CallWithLog(glEnable(GL_BLEND));
+        CallWithLog(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         //Vertex array object creation
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         VertexLayout layout;
         layout.Push(GL_FLOAT, 2);
+        layout.Push(GL_FLOAT, 2);
+
         VertexArray vao;
         vao.AddBuffer(vb, layout);
+
         //Creating IndexBuffer
         IndexBuffer ib(indices, 6);
 
@@ -57,15 +65,20 @@ int main(void)
         Shader myShader("res/shaders/Basic.shader");
         myShader.CreateShaderProgram();
 
+        //Textures
+        Texture texture("res/textures/AMLogo.png");
+        texture.Bind();
+
         Renderer renderer;
         float r = 0.0f, increment = 0.05f;
-
         //Main loop the draws on screen.
         while (!glfwWindowShouldClose(window)) {
             
             renderer.Clear();
             myShader.Bind();
             myShader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            myShader.SetUniform1i("u_Texture", 0);
+
             renderer.Draw(vao, ib, myShader);
 
             if (r > 1.0f)
